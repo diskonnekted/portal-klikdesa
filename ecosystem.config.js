97b2bd1d6bee1a89d8da1e2827e088e6c2953ecf
diskonnekted <arif.susilo@gmail.com>
2026-05-16 18:02:59 +0700

    config: reduce pm2 instances to 1 and use fork mode for memory efficiency

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
             name: "app", // The name of your application
             script: "node_modules/next/dist/bin/next", // The script to run
             args: "start", // Arguments for the script
-            instances: "4", // Launch 32 instances as CPU cores
-            exec_mode: "cluster", // Enable cluster mode
+            instances: "1", // Run only one instance to save memory
+            exec_mode: "fork", // Use fork mode for single instance
             autorestart: true, // Restart the app if it crashes
             watch: false, // Do not watch for file changes (we handle builds manually)
             max_memory_restart: "8G", // Restart if it exceeds 1GB of memory
